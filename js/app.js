75b6a9bf49fde96e7530eda0dc42a707a8824999
IDDesignSpace <42154502+IDDesignSpace@users.noreply.github.com>
2019-01-14 10:36:25 -0500

    Created functionality to keep player within bounds of the game. Enemy LadyBugs now move at different speeds.

--- js/app.js
+++ js/app.js
@@ -10,6 +10,9 @@
  *   
 */
 
+
+
+// Ian's comment- This begins the functionality for the enemy 
 // Enemies our player must avoid
 var Enemy = function(x,y,speed) {
     // Variables applied to each of our instances go here,
@@ -32,7 +35,29 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    console.log(dt);
+
+    // Ian's comment- This creates the movement of the ladybugs.
+    this.x += this.speed * dt;
+    // allEnemies[1] = 120
+
+    // Ian's comments- Create a row of three enemies.
+    allEnemies[0].y = enemyCoordinates[0];
+    allEnemies[1].y = enemyCoordinates[1];
+    allEnemies[2].y = enemyCoordinates[2];
+
+
+    // Ian's comment- if enemy x coordinate reaches the end of the board x (505), x should be reset to 0;
+    for (enemy of allEnemies) {
+        if (enemy.x > 505) {
+            this.x = -60;
+            enemy.randomSpeed();
+            console.log("reset ladybug");
+        }
+    }
+
+    // console.log(`Enemys time is ${dt}`);
+
+
 
 };
 
@@ -41,9 +66,45 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// Ian's comment- 
+Enemy.prototype.randomSpeed = function() {
+    var randomNum = Math.floor(Math.random() * (10 - 5) + 1);
+    this.speed = 50 * randomNum;  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+
+// Ian's comment- This begins the functionality for the player
 
 
 //  Ian's comment- creates player object and assigns initial x,y coordinates and 
@@ -57,7 +118,10 @@ var Player = function(x,y,speed) {
 //  Ian's comment- Update the player's information over and over again is called in the updateEntities function
 Player.prototype.update = function(dt) {
 
-    console.log(dt);
+    // console.log(dt);
+
+    // Ian's comment- Three rows of ladybug enemies generated
+
 
     
 };
@@ -70,31 +134,40 @@ Player.prototype.render = function () {
 
 Player.prototype.handleInput = function(keypress) {
 
-    // Ian's comment- This switch statement handles the key press in order to move the figure across the screen
+    // Ian's comment- This switch statement handles the key press in order to move the figure across the screen, it also sets the limits of the player. If a player reaches the end of the board the players coordinate will no longer be incremented in that direction. If the player reaches the water a function is called and the player has completed the round.
     switch (keypress) {
         case 'left': 
+        if (this.x === 0 ) { 
+            this.x = 0 
+        } else { 
         this.x -=  50 + this.speed;
+        }
         break;
         case 'up': 
+        if (this.y === -20) {
+            playerWins();
+        } else {
         this.y -= 30 + this.speed;
+        } 
         break;
         case 'right': 
+        if (this.x === 400) {
+            this.x = 400;
+        } else {
         this.x += 50 + this.speed;
+        }
         break;
         case 'down': 
+            if (this.y === 380) {
+                this.y = 380;
+            } else {
         this.y += 30 + this.speed;
+            }
         break;
     }
 
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-var allEnemies = [];
-
-var player = new Player(200,380,50);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -109,8 +182,28 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
-// Ian's comment- This for loop creates 5 new Enemy Objects and adds them to the allEnemies array 
-var numEnemies = 5;
+
+
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+
+var allEnemies = [];
+
+var enemyCoordinates = [60, 145, 225];
+
+var player = new Player(200, 380, 50);
+
+
+
+// Ian's comment- This for loop creates 3 new Enemy Objects and adds them to the allEnemies array 
+var numEnemies = 3;
+
 for(var i = 0;i < numEnemies; i++) {
-    allEnemies.push(new Enemy());
+    enemy = new Enemy(-60,60,120);
+    allEnemies.push(enemy);
+    enemy.randomSpeed();
 }
+
+// Ian's comment- I need to create a random number generator
